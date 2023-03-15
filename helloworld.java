d15ee90ec8552276a356bf41f24ea4e9f2c55707
Satyam Gupta <78143220+satyamgupta0@users.noreply.github.com>
2023-03-15 09:58:52 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("**************************#\n This was changed to check the behaviour of webhook in Jenkins.\n #########################");
+    System.out.println("Ho Ho Ho christmas is here.\n #########################");
     }
 }
